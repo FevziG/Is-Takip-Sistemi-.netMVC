b0911779ac904ce10fdc16426ba83189a8ff2862
Fevzi Güler <fvz_glr@hotmail.com>
2025-03-21 08:37:50 +0300

    IsTakipSistemiMVC(21.03.2025)

---
+++ IsTakipSistemiMVC(21.03.2025)/IsTakipSistemiMVC/Models/IsTakipViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace IsTakipSistemiMVC.Models
+{
+    public class IsTakipViewModel
+    {
+        public Personeller Personel { get; set; }
+        public List<IsDetayViewModel> Isler { get; set; }
+    }
+    public class IsDetayViewModel
+    {
+        public int IsId { get; set; }
+        public string IsBaslik { get; set; }
+        public string IsAciklama { get; set; }
+        public int? IsDurumId { get; set; }
+        public DateTime? IletilenTarih { get; set; }
+        public DateTime? YapilanTarih { get; set; }
+        public string IsYorum { get; set; }
+        public string DurumAd { get; set; }
+    }
+}
---
+++ IsTakipSistemiMVC(21.03.2025)/IsTakipSistemiMVC/Models/YetkiTurler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsTakipSistemiMVC.Models;
+
+public partial class YetkiTurler
+{
+    public int YetkiTurId { get; set; }
+
+    public string? YetkiTurAd { get; set; }
+
+    public virtual ICollection<Personeller> Personellers { get; set; } = new List<Personeller>();
+}
