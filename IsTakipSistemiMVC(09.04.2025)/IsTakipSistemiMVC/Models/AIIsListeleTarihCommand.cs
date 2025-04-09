1f04ca3262e8dd197a53f99786cd0a03662719fe
Fevzi Güler <fvz_glr@hotmail.com>
2025-04-09 18:04:40 +0300

    IsTakipSistemiMVC(09.04.2025)

---
+++ IsTakipSistemiMVC(09.04.2025)/IsTakipSistemiMVC/Models/AIIsListeleTarihCommand.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IsTakipSistemiMVC.Models
+{
+    public class AIIsListeleTarihCommand
+    {
+      public string action {get;set;}
+      public string kullaniciAdi { get;set;}
+      public DateTime? tarih {  get;set;}
+    }
+}
---
+++ IsTakipSistemiMVC(09.04.2025)/IsTakipSistemiMVC/Models/Isler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsTakipSistemiMVC.Models;
+
+public partial class Isler
+{
+    public int IsId { get; set; }
+
+    public string? IsBaslik { get; set; }
+
+    public string? IsAciklama { get; set; }
+
+    public int? IsPersonelId { get; set; }
+
+    public DateTime? IletilenTarih { get; set; }
+
+    public DateTime? YapilanTarih { get; set; }
+
+    public int? IsDurumId { get; set; }
+
+    public string? IsYorum { get; set; }
+
+    public bool IsOkunma { get; set; }
+
+    public virtual Durumlar? IsDurum { get; set; }
+
+    public virtual Personeller? IsPersonel { get; set; }
+}
